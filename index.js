7d110b21a8b1739ccd38b1122ee6e08b86aff42b
HBIN12 <1135911642@qq.com>
2022-06-19 09:59:37 +0800

    样式更新

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app = new express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
